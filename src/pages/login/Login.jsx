a7eeb4f7222ac5149cc75911f8025abf99893674
Angela Toral <158821520+AngToral@users.noreply.github.com>
2024-08-07 21:52:30 +0200

    sass y login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,12 +1,70 @@
-
+import { useEffect } from "react";
+import { Button, Checkbox, Form, Input, Flex } from 'antd';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import './login.scss'
 
 function Login() {
+    useEffect(() => {
+        document.body.style.backgroundColor = "#f0ffff"
+    })
+
+    const onFinish = (values) => {
+        console.log('Received values of form: ', values);
+    };
+
     return (
         <>
             <div>
                 <h1>
                     Hola, login
                 </h1>
+                <Form
+                    name="login"
+                    initialValues={{
+                        remember: true,
+                    }}
+                    style={{
+                        maxWidth: 360,
+                    }}
+                    onFinish={onFinish}
+                >
+                    <Form.Item
+                        name="email"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Username!',
+                            },
+                        ]}
+                    >
+                        <Input prefix={<UserOutlined />} placeholder="Email" />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Password!',
+                            },
+                        ]}
+                    >
+                        <Input.Password prefix={<LockOutlined />} type="password" placeholder="Password" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Flex justify="space-between" align="center">
+                            <Form.Item name="remember" valuePropName="checked" noStyle>
+                                <Checkbox>Remember me</Checkbox>
+                            </Form.Item>
+                            <a href="">Forgot password</a>
+                        </Flex>
+                    </Form.Item>
+
+                    <Form.Item>
+                        <Button className="button" block type="primary" htmlType="submit">
+                            Log in
+                        </Button>
+                    </Form.Item>
+                </Form>
             </div>
         </>
     );
