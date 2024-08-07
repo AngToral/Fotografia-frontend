a6ff462f34f4b00851c91d68b81f267a2de1362e
Angela Toral <158821520+AngToral@users.noreply.github.com>
2024-08-07 15:19:04 +0200

    apis

---
+++ src/apiService/entryApi.js
@@ -0,0 +1,57 @@
+const baseUrl = import.meta.env.VITE_BACKEND;
+
+export const getPhoto = async () => {
+    const token = localStorage.getItem("access_token");
+    const response = await fetch(`${baseUrl}/gallery`, {
+        headers: { authorization: `Bearer ${token}` }, // ponemos en headers el token generado
+    });
+    const entry = await response.json();
+    return entry;
+};
+
+export const getPhotoId = async (id) => {
+    const token = localStorage.getItem("access_token");
+    const response = await fetch(`${baseUrl}/gallery/${id}`, {
+        headers: { authorization: `Bearer ${token}` }, // ponemos en headers el token generado
+    });
+    const entry = await response.json();
+    return entry;
+};
+
+export const addPhoto = async (data) => {
+    const token = localStorage.getItem("access_token");
+    const response = await fetch(`${baseUrl}/gallery`, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json",
+            "authorization": `Bearer ${token}`,
+        },
+    });
+    const entry = await response.json();
+    return entry;
+};
+
+export const updatePhoto = async (id, data) => {
+    const token = localStorage.getItem("access_token");
+    const response = await fetch(`${baseUrl}/gallery/${id}`, {
+        method: "PUT",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json",
+            authorization: `Bearer ${token}`,
+        },
+    });
+    const entry = await response.json();
+    return entry;
+};
+
+export const deletePhoto = async (id) => {
+    const token = localStorage.getItem("access_token");
+    const response = await fetch(`${baseUrl}/absences/${id}`, {
+        method: "DELETE",
+        headers: { authorization: `Bearer ${token}` },
+    });
+    const entry = await response.json();
+    return entry;
+};
