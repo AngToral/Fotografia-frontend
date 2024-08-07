457088c21e3f9efd48714a9f6efa4ead1ebb5213
Angela Toral <158821520+AngToral@users.noreply.github.com>
2024-08-07 15:39:24 +0200

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Login from './pages/login/Login'
+import Home from './pages/home/Home'
 
 function App() {
 
   return (
     <>
-
+      <Routes>
+        <Route path='*' element={<Navigate to='/' />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Home />} />
+      </Routes>
     </>
   )
 }
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+
+
+function Home() {
+    return (
+        <>
+            <div>
+                <h1 className="text-3xl font-bold">
+                    Hola, home
+                </h1>
+            </div>
+        </>
+    );
+}
+
+export default Home;
