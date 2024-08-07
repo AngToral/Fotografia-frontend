4a110877140256029bab3063f3b629d6da8724f5
Angela Toral <158821520+AngToral@users.noreply.github.com>
2024-08-07 20:30:30 +0200

    tailwindcss

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,15 @@
+import { useEffect } from "react";
 
 
 function Home() {
+    useEffect(() => {
+        document.body.style.backgroundColor = "#f0ffff"
+    })
+
     return (
         <>
             <div>
-                <h1 className="text-3xl font-bold">
+                <h1 className="text-3xl font-bold text-foto-100">
                     Hola, home
                 </h1>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,28 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        foto: {
+          50: "#transparent", //trasparente
+          100: "#d496a7", //rosa oscuro
+        },
+      },
+    },
   },
   plugins: [],
-}
+});
