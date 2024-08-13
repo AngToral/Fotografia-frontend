ae379b4a41a27c546e7f00cd9bedaae429eae808
Angela Toral <158821520+AngToral@users.noreply.github.com>
2024-08-13 20:20:14 +0200

    home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import './home.scss'
-import { Button, Flex, } from "antd";
+import { Button, Divider, Flex, Typography, } from "antd";
 import {
     Menu,
     MenuHandler,
@@ -15,33 +15,41 @@ function Home() {
 
     return (
         <>
-            <div className="inicio h-screen">
+            <div className="h-screen">
                 <Flex justify="space-between">
                     <Flex>
-                        <Button type="text" className="m-4 lg:text-xl font-bold text-foto-800">
+                        <Button type="text" className="m-4 md:text-xl font-bold text-foto-800">
                             Sobre mí
                         </Button>
-                        <Button type="text" className="m-4 lg:text-xl font-bold text-foto-800">
+                        <Button type="text" className="m-4 md:text-xl font-bold text-foto-800">
                             Contacto
                         </Button>
                     </Flex>
                     <Menu>
                         <MenuHandler>
-                            <Button type="text" className="m-4 lg:text-xl font-bold text-foto-800">Idioma</Button>
+                            <Button type="text" className="m-4 md:text-xl font-bold text-foto-800">Idioma</Button>
                         </MenuHandler>
-                        <MenuList>
-                            <MenuItem>🇪🇸 Español</MenuItem>
-                            <MenuItem>🇺🇸 English</MenuItem>
+                        <MenuList className="bg-foto-700 border-foto-100">
+                            <MenuItem className="text-black">🇪🇸 Español</MenuItem>
+                            <MenuItem className="text-black">🇺🇸 English</MenuItem>
                         </MenuList>
                     </Menu>
                 </Flex>
-                <Flex vertical align="center" justify="center" className="h-full">
-                    <div>
-                        <h1 className="text-3xl font-bold text-foto-800">Firma centro abajo</h1>
-                    </div>
-                    <div>
-                        <h1 className="text-3xl font-bold text-foto-800">Foto centro debajo</h1>
-                    </div>
+                <Flex vertical align="center" justify="center" className="">
+                    <img className="firma" src="../../../images/firma.png" />
+                    <img className="foto-inicio sm:h-[490px]" src="../../../images/perfil.png" />
+                    <p className="text-foto-800">_______________</p>
+                    <Flex>
+                        <Button type="text" className="md:text-xl text-foto-800">
+                            Instagram
+                        </Button>
+                        <Button type="text" className="md:text-xl text-foto-800">
+                            Portfolio
+                        </Button>
+                        <Button type="text" className="md:text-xl text-foto-800">
+                            Blog
+                        </Button>
+                    </Flex>
                 </Flex>
             </div>
             <div className="inicio flex justify-center items-center h-screen">
