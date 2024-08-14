e137e3ed6df7f8c651c468712b3b9946b9fb4441
Angela Toral <158821520+AngToral@users.noreply.github.com>
2024-08-14 22:34:26 +0200

    scroll en home

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,30 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { I18nextProvider } from "react-i18next";
+import i18next from 'i18next';
+import home_es from "./translations/es/home.json"
+import home_en from "./translations/en/home.json"
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "es",
+  resources: {
+    es: {
+      home: home_es
+    },
+    en: {
+      home: home_en
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <I18nextProvider i18n={i18next}>
+        <App />
+      </I18nextProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,66 +1,92 @@
 import { useEffect, useState } from "react";
 import './home.scss'
-import { Button, Divider, Flex, Typography, } from "antd";
+import { Button } from "antd";
 import {
     Menu,
     MenuHandler,
     MenuList,
     MenuItem,
 } from "@material-tailwind/react";
+import { useTranslation } from "react-i18next";
+import { FaAngleDown } from "react-icons/fa";
+import { Animator, ScrollContainer, ScrollPage, batch, Fade, FadeIn, FadeOut, Move, MoveIn, MoveOut, Sticky, StickyIn, StickyOut, Zoom, ZoomIn, ZoomOut } from "react-scroll-motion";
+
 
 function Home() {
     useEffect(() => {
         document.body.style.backgroundColor = "#F1F0E8"
     })
+    const [t, i18n] = useTranslation("home")
+
+    const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
+    const FadeUp = batch(Fade(), Move(), Sticky());
 
     return (
         <>
-            <div className="h-screen big-screen">
-                <div className="flex justify-end">
-                    <Button type="text" className="m-4 md:text-xl text-base font-bold text-foto-900">
-                        Sobre mí
-                    </Button>
-                    <Button type="text" className="m-4 md:text-xl text-base font-bold text-foto-900">
-                        Contacto
-                    </Button>
-                    <Menu>
-                        <MenuHandler>
-                            <Button type="text" className="m-4 md:text-xl text-base font-bold text-foto-900">Idioma</Button>
-                        </MenuHandler>
-                        <MenuList className="bg-foto-700 border-foto-100">
-                            <MenuItem className="text-black" data-lenguage="es">🇪🇸 Español</MenuItem>
-                            <MenuItem className="text-black" data-lenguage="en">🇬🇧 English</MenuItem>
-                        </MenuList>
-                    </Menu>
+            {/* header */}
+            <ScrollContainer>
+                <ScrollPage>
+                    <Animator animation={Fade()}>
+                        <div className="h-screen big-screen">
+                            <div className="flex justify-end">
+                                <Button type="text" className="m-4 md:text-xl text-base font-bold text-foto-900">
+                                    {t("header.about-me")}
+                                </Button>
+                                <Button type="text" className="m-4 md:text-xl text-base font-bold text-foto-900">
+                                    {t("header.contact")}
+                                </Button>
+                                <Menu>
+                                    <MenuHandler>
+                                        <Button type="text" className="m-4 md:text-xl text-base font-bold text-foto-900">
+                                            {t("header.lenguage")}<FaAngleDown />
+                                        </Button>
+                                    </MenuHandler>
+                                    <MenuList className="bg-foto-700 border-foto-100">
+                                        <MenuItem onClick={() => i18n.changeLanguage("es")} className="text-black" data-lenguage="es">🇪🇸 Español</MenuItem>
+                                        <MenuItem onClick={() => i18n.changeLanguage("en")} className="text-black" data-lenguage="en">🇬🇧 English</MenuItem>
+                                    </MenuList>
+                                </Menu>
+                            </div>
+                            <div className="flex flex-col items-center">
+                                <img className="imagen rounded-full h-[300px]" src="../../../images/perfil-cuadrado.png" alt="mariana-mendoza" />
+                                <img className="firma absolute" src="../../../images/firma-verde.png" alt="mariana-mendoza" />
+                                <h1 type="text" className="encabezado text-7xl text-foto-900 font-display">
+                                    {t("header.fotographer")}
+                                </h1>
+                                <p className="text-foto-900">__________________________________</p>
+                                <div className="flex">
+                                    <Button type="text" className="text-lg text-foto-900">
+                                        {t("header.gallery")}
+                                    </Button>
+                                    <Button href="https://www.instagram.com/nanamendozago/" target="_blanck" type="text" className="text-lg text-foto-900">
+                                        Instagram
+                                    </Button>
+                                    <Button type="text" className="text-lg text-foto-900">
+                                        Portfolio
+                                    </Button>
+                                    <Button type="text" className="text-lg text-foto-900">
+                                        Blog
+                                    </Button>
+                                </div>
+                            </div>
+                        </div>
+                    </Animator>
+                </ScrollPage>
+                {/* servicios */}
+                <ScrollPage>
+                    <Animator animation={batch(Fade(), Sticky())}>
+                        <p style={{ fontSize: "30px" }}>Servicios</p>
+                    </Animator>
+                </ScrollPage>
+                {/* contacto */}
+                <div>
+                    <p style={{ fontSize: "30px" }} className="prueba h-screen">Contacto</p>
                 </div>
-                <div className="flex flex-col items-center">
-                    <img className="imagen rounded-full h-[300px]" src="../../../images/perfil-cuadrado.png" alt="mariana-mendoza" />
-                    <img className="firma absolute" src="../../../images/firma-verde.png" alt="mariana-mendoza" />
-                    <h1 type="text" className="encabezado text-7xl text-foto-900 font-display">
-                        Fotógrafa
-                    </h1>
-                    <p className="text-foto-900">__________________________________</p>
-                    <div className="flex">
-                        <Button type="text" className="text-lg text-foto-900">
-                            Galería
-                        </Button>
-                        <Button type="text" className="text-lg text-foto-900">
-                            Instagram
-                        </Button>
-                        <Button type="text" className="text-lg text-foto-900">
-                            Portfolio
-                        </Button>
-                        <Button type="text" className="text-lg text-foto-900">
-                            Blog
-                        </Button>
-                    </div>
+                {/* footer */}
+                <div >
+                    <p style={{ fontSize: "30px" }} className="prueba h-[100px]">Footer</p>
                 </div>
-            </div>
-            <div className="">
-                <h1 className="text-3xl font-bold text-foto-100">
-                    Seguimos
-                </h1>
-            </div>
+            </ScrollContainer >
         </>
     );
 }
