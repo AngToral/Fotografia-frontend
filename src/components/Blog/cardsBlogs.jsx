d57996f2dd5344dc8f3588c485c44c00a01e3e14
Angela Toral <158821520+AngToral@users.noreply.github.com>
2024-09-24 22:32:14 +0200

    eliminar entrada de blog

--- src/components/Blog/cardsBlogs.jsx
+++ src/components/Blog/cardsBlogs.jsx
@@ -1,14 +1,20 @@
-import { Button, Card, CardBody, CardFooter, CardHeader, Typography } from "@material-tailwind/react";
+import { Button, Card, CardBody, CardFooter, CardHeader, Popover, PopoverContent, PopoverHandler, Typography } from "@material-tailwind/react";
+import { message } from "antd";
+import { deleteEntry } from "../../apiService/entryApi";
 
 
 const CardsBlogs = ({ entry, refresh }) => {
 
-    const onDelete = async () => {
-        console.log("elimino id: ", entry._id)
+    const onDelete = async (id) => {
+        console.log("elimino id: ", id)
+        await deleteEntry(id)
+        refresh(prev => !prev);
+        message.success("Photo deleted!")
     }
 
-    const onEdit = async () => {
-        console.log("edito id: ", entry._id)
+    const onEdit = async (id) => {
+        console.log("edito id: ", id)
+
     }
 
     return (
@@ -30,8 +36,16 @@ const CardsBlogs = ({ entry, refresh }) => {
                         </Typography>
                     </CardBody>
                     <CardFooter className="pt-0 flex justify-around">
-                        <Button className="bg-foto-400 text-foto-200" onClick={onEdit}>Editar</Button>
-                        <Button className="bg-foto-400 text-foto-200" onClick={onDelete}>Eliminar</Button>
+                        <Button className="bg-foto-400 text-foto-200" onClick={() => onEdit(entry._id)}>Edit</Button>
+                        <Popover>
+                            <PopoverHandler>
+                                <Button className="bg-foto-400 text-foto-200">Delete</Button>
+                            </PopoverHandler>
+                            <PopoverContent className="flex flex-col">
+                                Are you sure?
+                                <Button className="bg-foto-200 text-foto-400 mt-3" onClick={() => onDelete(entry._id)}>Yes!</Button>
+                            </PopoverContent>
+                        </Popover>
                     </CardFooter>
                 </Card>
             </div>
