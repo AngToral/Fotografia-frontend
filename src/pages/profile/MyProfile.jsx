decab21b3ccc0f420090575c45850e9c188357f6
Angela Toral <158821520+AngToral@users.noreply.github.com>
2024-10-02 19:01:20 +0200

    fondo de perfil

--- src/pages/profile/MyProfile.jsx
+++ src/pages/profile/MyProfile.jsx
@@ -6,7 +6,7 @@ import { IoIosLogOut } from "react-icons/io";
 function MyProfile() {
 
     useEffect(() => {
-        document.body.style.backgroundImage = "linear-gradient(rgba(255, 255, 255, 0.40), rgba(240, 255, 255, 0.40)), url('../../../../images/fondo-profile.png')";
+        document.body.style.backgroundImage = "linear-gradient(rgba(255, 255, 255, 0.0), rgba(240, 255, 255, 0.10)), url('../../../../images/fondo-profile.png')";
     }, []);
 
     const navigate = useNavigate();
@@ -20,26 +20,32 @@ function MyProfile() {
             <div className="h-screen">
                 <div className="flex justify-between flex-wrap">
                     <div className="flex justify-start mx-6">
-                        <button variant="text" className="link font-display text-foto-900 m-4 md:text-xl font-bold" onClick={handleHome}>
+                        <button variant="text" className="link2 font-display text-foto-200 m-4 md:text-xl font-bold" onClick={handleHome}>
                             Home
                         </button>
                     </div>
                     <div className="flex justify-end mx-6">
-                        <button variant="text" className="flex items-center link font-display text-foto-900 m-4 md:text-xl font-bold">
+                        <button variant="text" className="flex items-center link2 font-display text-foto-200 m-4 md:text-xl font-bold">
                             <IoIosLogOut className="mr-2" /> Logout
                         </button>
                     </div>
                 </div>
                 <div>
-                    <Typography className="m-8">
-                        Cambiar contraseña
-                    </Typography>
-                    <Typography className="m-8">
-                        Cambiar email
-                    </Typography>
-                    <Typography className="m-8">
-                        Enviar correo de nueva reseña
-                    </Typography>
+                    <div className="m-8">
+                        <Typography className="font-display font-bold md:text-2xl">
+                            Change password
+                        </Typography>
+                    </div>
+                    <div className="m-8">
+                        <Typography className="font-display font-bold md:text-2xl">
+                            Change email
+                        </Typography>
+                    </div>
+                    <div className="m-8">
+                        <Typography className="font-display font-bold md:text-2xl">
+                            Send new review email
+                        </Typography>
+                    </div>
                 </div>
             </div>
         </>
